896314dfa808209a542bc7dc38d95d1c87e1eed1
justyna <strawberka17@gmail.com>
2022-07-12 21:32:38 +0100

    done script.js

--- progress steps/script.js
+++ progress steps/script.js
@@ -1,26 +1,45 @@
 const circles =document.querySelectorAll('.circle')
-const next =document.querySelectorAll('next')
-const prev =document.querySelectorAll('prev')
-
-
+const next =document.getElementById('next')
+const prev =document.getElementById('prev')
+const progress=document.getElementById('progress')
+debugger
 let currentActive =1
 
-next.addEventlistener('click',()=>{
+next.addEventListener('click',()=>{
     currentActive++
     if (currentActive > circles.length){
-        currentActive === circles.length
+        currentActive = circles.length
     }
    update () 
 })
 
-prev.addEventlistener('click',()=>{
+prev.addEventListener('click',()=>{
     currentActive --
     if(currentActive <  1){
         currentActive=1
     }
-    update
+    update()
 })
 
 function update (){
+circles.forEach((circle ,idx)=>{
+if(idx< currentActive){
+    circle.classList.add('active')
+}else{
+    circle.classList.remove('active')
+}
+    
+})
 
+const actives=document.querySelectorAll('.active')
+progress.style.width =(actives.length -1) /(circles.length -1) *100 + '%'
+
+if(currentActive === 1) {
+    prev.disabled =true
+}else if(currentActive === circles.length){
+    next.disabled=true
+}else {
+    prev.disabled=false
+    next.disabled=false
+}
 }
