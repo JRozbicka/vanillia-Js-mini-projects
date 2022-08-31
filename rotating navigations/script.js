cfb163dc472d095831a1a960ff45c25d8687a429
justyna <strawberka17@gmail.com>
2022-08-31 18:17:54 +0100

    done script

---
+++ rotating navigations/script.js
@@ -0,0 +1,13 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
+function openNav() {
+  container.classList.add("show-nav");
+}
+
+function closeNav() {
+  container.classList.remove("show-nav");
+}
+
+open.addEventListener("click", openNav);
+close.addEventListener("click", closeNav);
